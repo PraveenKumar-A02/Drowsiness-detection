4c503371a3e78664f878cc43d15d37b4611e8634
PraveenKumar-A02 <138895886+PraveenKumar-A02@users.noreply.github.com>
2023-10-17 18:39:26 +0530

    Update main.py
    inserted some commands

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ import imutils
 import dlib
 import cv2
 
+#To play the Alert sount
 mixer.init()
 mixer.music.load("alert_sound.mp3")
 
-
 def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
     A = distance.euclidean(eye[1], eye[5])
     B = distance.euclidean(eye[2], eye[4])
+    # compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
     C = distance.euclidean(eye[0], eye[3])
+    # compute the eye aspect ratio
     ear = (A + B) / (2.0 * C)
     return ear
 
